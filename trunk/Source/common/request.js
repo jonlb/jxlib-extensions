4274c882d519ee533e96b35d071fba56d0938386
jonlb <jonlb@comcast.net>
2010-01-16 16:22:47 -0800

    update to add Editor widget and plugins

--- trunk/Source/common/request.js
+++ trunk/Source/common/request.js
@@ -44,34 +44,49 @@
  */
 Jx.Request = new Class({
 	
-	Implements: [Events, Options],
+	Extends: Jx.Object,
 	
-	requests: new Array(),
+	requests: [],
 	requestCount: 0,
 	loadingVisible: false,
 	errorHandlers: null,
 	processRequests: true,
+	running: false,
 	
 	options: {
 		//events - i.e. callbacks for this request
-		onSuccess: $empty,		//fired on receiving a success=true from the server
-		onFailure: $empty,		//fired if mootools request fires a failure
-		onDebug: $empty,		//used to display debug info if set
-		onError: $empty,		//fired on receiving a success=false from the server
+        //events: {
+		   // onSuccess: $empty,		//fired on receiving a success=true from the server
+		    //onFailure: $empty,		//fired if mootools request fires a failure
+		    //onDebug: $empty,		//used to display debug info if set
+		    //onError: $empty		//fired on receiving a success=false from the server
+        //},
 		
 		//flags
 		debug: false,
-		showLoading: false,
+		showLoading: {
+            el: null,
+            opts: {
+                baseHref: '',
+                img: {
+                    src: '/images/load.gif',
+                    styles: {
+                        width: 16,
+                        height: 16
+                    }
+                }
+            }
+        },
+		
 			
 		//options
-		loadingClass: 'ajax-loader',
-		loadingElementId: 'ajax-loader',
 		url: null,
 		method: 'get',
 		type: 'json',
 		update: null,
 		evalScripts: true,
-		evalResponse: false
+		evalResponse: false,
+		data: {}
 		
 	},
 	
@@ -85,7 +100,7 @@ Jx.Request = new Class({
 	 * returns: 
 	 * A hash including the options and the request
 	 */
-	init: function(options){
+	init: function (options) {
 		
 		//merge the passed options with the base options
 		var opts = $merge(this.options, options);
@@ -97,39 +112,44 @@ Jx.Request = new Class({
 		//create a hash to contain the info for this particular request
 		var r = new Hash({requestId: this.requestCount, options: opts});
 		
+		opts.data.requestId = r.get('requestId');
+		
 		//create the request object itself
 		var request = null;
 		if (opts.type === 'json') {
 			request = new Request.JSON({
 				url: opts.url,
+				data: opts.data,
 				onSuccess: this.processSuccess.bind(this),
-				onFailure: (function(instance){
+				onFailure: (function (instance) {
 					this.failure(instance, r.get('requestId'));
 				}).bind(this)
 			});
-		} else if (opts.type === 'html'){
+		} else if (opts.type === 'html') {
 			request = new Request.HTML({
 				url: opts.url,
 				update: opts.update,
+				data: opts.data,
 				evalScripts: opts.evalScripts,
-				onSuccess: (function(responseTree, responseElements, responseHTML, responseJavaScript){
+				onSuccess: (function (responseTree, responseElements, responseHTML, responseJavaScript) {
 					this.success(r.get('requestId'), [responseTree, responseElements, responseHTML, responseJavaScript]);
 				}).bind(this),
-				onFailure: (function(instance){
+				onFailure: (function (instance) {
 					this.failure(instance, r.get('requestId'));
 				}).bind(this)
 			});
 		} else {
 			request = new Request({
 				url: opts.url,
+				data: opts.data,
 				evalResponse: opts.evalResponse,
-				onSuccess: (function(responseText, responseXML){
+				onSuccess: (function (responseText, responseXML) {
 					this.success(r.get('requestId'), [responseText, responseXML]);
 				}).bind(this),
 				onFailure: this.failure.bind(this)
-			})
+			});
 		}
-		r.combine({'request':request});
+		r.combine({'request': request});
 		
 		return r;
 	},
@@ -142,13 +162,15 @@ Jx.Request = new Class({
 	 * instance - the request instance itself
 	 * id - the id of the request
 	 */
-	failure: function(instance, id){
+	failure: function (instance, id) {
 		var req = this.getRequest(id);
 		var opt = req.get('options');
 		this.removeMask(opt);
 		if ($defined(opt.events) && $defined(opt.events.onFailure)) {
     		opt.events.onFailure.run(instance);
     	}
+		this.running = false;
+		this.run();
 	},
 	
 	/**
@@ -159,13 +181,17 @@ Jx.Request = new Class({
 	 * id - the id of the request
 	 * args - the arguments sent to the success function by the request [responseTree, responseElements, responseHTML, responseJavaScript]
 	 */
-	success: function(id, args) {
+	success: function (id, args) {
 		var req = this.getRequest(id);
 		var opt = req.get('options');
-		this.removeMask(opt);
+		this.removeMask(req);
+		this.running = false;
+		this.run();
 		if ($defined(opt.events) && $defined(opt.events.onSuccess)) {
         	opt.events.onSuccess.run(args);
         }
+		
+		
 	},
 	
 	/**
@@ -179,47 +205,88 @@ Jx.Request = new Class({
 	 * Returns:
 	 * the request id
 	 */
-	callServer: function(data, options){
-		if (this.processRequests || options.process){
-			var d;
-			var req;
-			if ($type(options) != 'hash'){
-				req = this.init(options);
-			} else {
-				req = options;
-			}
-			var opts = req.get('options');
-			var request = req.get('request');
+	callServer: function (data, options, position, hold) {
+	    if (!$defined(position)) {
+	        position = 'bottom';
+	    }
+	    
+	    hold = $defined(hold) ? hold : false;
+	    
+		var d;
 			
-			if ($defined(data)){
-				type = $type(data);
-				if ( type != 'hash') {
-					d = new Hash(data);
-				} else {
-					d = data;
-				}
-			} else {
-					d = new Hash({'requestId': req.get('requestId')});
-			}
-			
-			if (!d.has('requestId')){
-				d.include('requestId',req.get('requestId'));
-			}
-			req.combine({'data':d});
-			this.requests.include(req);
-			//activate loading mask if requested
-			if (opts.showLoading && !this.loadingVisible) {
-				loadingEl = $(opts.loadingElementId);
-				loadingEl.addClass(opts.loadingClass);
-				this.loadingVisible = true;
-			}
-			if (opts.method == 'get') {
-				var r = request.get(opts.url, d.toQueryString());
-			} else {
-				var r = request.post(opts.url, d.toQueryString());
-			}
-			return req.get('requestId');
+		if ($defined(data)) {
+            var type = $type(data);
+            if (type !== 'hash') {
+                d = new Hash(data);
+            } else {
+                d = data;
+            }
+        } else {
+            if ($type(options) === 'hash') {
+                if (!options.has('data')) {
+                    d = new Hash();
+                } else {
+                    d = options.get('data');
+                }
+            } else {
+                d = new Hash();
+            }
+        }
+        
+		var req;
+		if ($type(options) !== 'hash') {
+		    options.data = d;
+			req = this.init(options);
+			req.set('data', d);
+		} else {
+			req = options;
 		}
+		
+		req.combine({'data': d});
+		if (position === 'top') {
+		    this.requests.unshift(req);
+		} else {
+		    this.requests.push(req);
+		}
+		
+		if (!hold) {
+		    this.run();
+		}
+		
+		return req.get('requestId');
+			    
+	},
+	
+	run: function (override) {
+	    override = ($defined(override)) ? override : false;
+	    //as long as a request isn't in process and stop() hasn't been called.
+	    if ((!this.running && this.processRequests && (this.requests.length > 0)) || override) {
+	        this.running = true;
+	        //we haven't dispatched a request yet... do one!
+	        var req = this.currentRequest = this.requests.shift();
+	        var opts = req.get('options');
+	        
+	        //activate loading mask if requested
+	        if (opts.showLoading.el && !req.has('waiter')) {
+                var w = new Spinner(opts.showLoading.el, opts.showLoading.opts);
+                w.start();
+                req.set('waiter', w);
+            } else if (opts.showLoading.el && req.has('waiter')) {
+                req.get('waiter').start();
+            }
+                
+            req.get('request').send();
+	    }
+	},
+	
+	/**
+	 * Method: runOne
+	 * Used to force only one request to run. Used when stop() was called
+	 * to stop processing the queue but we still need to call just 
+	 * the next request.
+	 */
+	runOne: function () {
+	    this.run(true);
 	},
 	
 	/**
@@ -233,45 +300,60 @@ Jx.Request = new Class({
 	 * Returns:
 	 * false on error, otherwise nothing
 	 */
-	processSuccess: function(data, text){
+	processSuccess: function (data, text) {
 		
 		//find the appropriate request hash
 		if ($defined(data)) {
-			var req = this.getRequest(data.requestId);
+			var req = this.currentRequest;
 			var options = req.get('options');
-			
+			this.removeMask();
+			this.running = false;
+	        this.run();
+	        
 			try {    		
 		    	//call debug first
 		    	if (options.debug && $defined(data.debug)) {
-	                	if ($defined(options.events.onDebug)) {
-	                		options.events.onDebug.run(data.debug);
-	                	}
+		    	    if ($defined(options.events.onDebug)) {
+	                	options.events.onDebug.run(data.debug);
+	                }
 		    	}
 		    	
-		    	if (data.success){
+		    	if (data.success) {
 		    		if ($defined(options.events.onSuccess)) {
 	            		options.events.onSuccess.run(data);
 	            	}
 		    	} else {
-		    		//first try to handle the error ourselves
-		    		if (!this.handleError(data, req, options)){
+		    		//first try to handle the error globally
+		    		if (!this.handleError(data, req, options)) {
 		    			//then pass everything to the registered error handler
 		    			if ($defined(options.events.onError)) {
 		    				options.events.onError.run(data);
 		    			}
 		    		}
 		    	}
-		    	//this.eventsRemove(options.events);
-		    	this.removeMask(options);
 		    } catch (err) {
 		    	//this is obviously an error that we didn't expect
-	    		alert('Error : ' + err.toString());
-	    		return false;
+		        //TODO: change this to a Jx.Dialog.Message class
+		        var msg = 'Error occurred: ' + err.message;
+		        msg += '<br/>in ' + err.fileName;
+		        msg += '<br/>at line ' + err.lineNumber;
+		        msg += '<br/>Stack Trace:<br/>' + err.stack;
+		        if ($defined(Jx.Dialog.Message)) {
+		            var dlg = new Jx.Dialog.Message({
+		                message: msg,
+		                width: 700,
+		                height: 400,
+		                resize: true
+		            });
+		            dlg.open();
+		        } else {
+		            alert(msg);
+		        }
 	    	}
 		} else {
 			console.log(text);
-			return false;
 		}
+		
 	    
 	},
 	
@@ -285,10 +367,10 @@ Jx.Request = new Class({
 	 * Returns:
 	 * the request, if found. null, if not found.
 	 */
-	getRequest: function(requestId){
+	getRequest: function (requestId) {
 		var req = null;
-		this.requests.each(function(item){
-			if (item.get('requestId') == requestId){
+		this.requests.each(function (item) {
+			if (item.get('requestId') === requestId) {
 				req = item;
 			}
 		}, this);
@@ -303,13 +385,13 @@ Jx.Request = new Class({
 	 * Removes the loading mask.
 	 * 
 	 * Parameters:
-	 * opts - the options for the request
+	 * req - the request hash
 	 */
-	removeMask: function(opts){
-		if (this.loadingVisible && (this.requests.length == 0)) {
-			loadingEl = $(opts.loadingElementId);
-			loadingEl.removeClass(opts.loadingClass);
-			this.loadingVisible = false;
+	removeMask: function () {
+		if (this.currentRequest.has('waiter')) {
+		    var w = this.currentRequest.get('waiter');
+		    w.stop();
+		    this.currentRequest.erase('waiter');
 		}
 	},
 	
@@ -325,14 +407,14 @@ Jx.Request = new Class({
 	 * Returns:
 	 * true if handled, false if not.
 	 */
-	handleError: function(data, req, opts){
+	handleError: function (data, req, opts) {
 		if ($defined(this.errorHandlers)) {
 			if ($defined(data.error)) {
 				var code = data.error.code;
 				var message = data.error.message;
-				if (this.errorHandlers.has(code)){
-					handler = this.errorHandlers.get(code);
-					handler.run([message,req]);
+				if (this.errorHandlers.has(code)) {
+					var handler = this.errorHandlers.get(code);
+					handler.run([message, req]);
 					this.removeMask(opts);
 					return true;
 				}
@@ -349,27 +431,29 @@ Jx.Request = new Class({
 	 * code - the error code this handler corresponds to
 	 * handler - the function to run to handle the error
 	 */
-	addErrorHandler: function(code, handler){
-		if (!$defined(this.errorHandlers)){
+	addErrorHandler: function (code, handler) {
+		if (!$defined(this.errorHandlers)) {
 			this.errorHandlers = new Hash();
 		}
-		this.errorHandlers.set(code,handler);
+		this.errorHandlers.set(code, handler);
 	},
 	
 	/** 
 	 * Method: stop
 	 * called to stop processing requests
 	 */
-	stop: function(){
+	stop: function () {
 		this.processRequests = false;
 	},
 	
 	/** 
 	 * Method: start
-	 * called to start processing requests after a call to <stop>
+	 * called to start processing requests after a call to <stop>.
+	 * Immediately begins running.
 	 */
-	start: function(){
+	start: function () {
 		this.processRequests = true;
+		this.run();
 	}
 });
 
